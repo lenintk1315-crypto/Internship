4bd94da6a903a3401debb0002360555bb257bbfa
Lenin T K <lenintk1315@gmail.com>
2026-08-03 11:48:41 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,7 @@
+username=input("Enter the username\n")
+password=input("Enter the password\n")
+if username=="admin" and password=="1234":
+    print("login successfull")
+else:
+    print("login failed")
+
