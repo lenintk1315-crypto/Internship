7f0bdf0e50906218c1fa6be13574a2c6847937c5
Lenin T K <lenintk1315@gmail.com>
2026-08-04 20:06:20 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,29 @@
+num=int(input("Enter the number:"))
+fact=1
+for i in range(1,num+1):
+    fact=fact*i
+    print("The factorial is:",fact)
+
+
+
+
+
+
+
+
+# //////
+# # def factorial(num):
+#     fact=1
+#     for i in range(1,num+1):
+#         fact=fact*i
+#     return fact
+# num=int(input("Enter the number:"))
+# print("The fctorial is:",factorial(num))
+# //////
+# def factorial(num):
+#     if  num==1:
+#         return 1
+#     else:
+#         return num*factorial(num-1)
+# num=int(input("Enter the number:"))
+# print("The factorial is:",factorial(num))
