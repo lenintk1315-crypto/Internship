498256da7f50fb12903939b483a48fb39a02099c
Lenin TK <lenintk1315@gmail.com>
2026-08-05 10:00:19 +0530

    Add Logical_Operations and Conditionals

---
+++ Logical_Operations and Conditionals/comparison_operator.py
@@ -0,0 +1,12 @@
+def comparison():
+    var1=int(input("Enter first number: "))
+    var2=int(input("Enter second number: "))
+    if var1>var2:
+        print(f"{var1} is greather than {var2}")
+    elif var1<var2:
+        print(f"{var1}less than {var2}")
+    else:
+        print(f"{var1} is equal to {var2}")
+comparison()
+
+
---
+++ Logical_Operations and Conditionals/compound_boolean_logic.py
@@ -0,0 +1,11 @@
+def leapyear(year):
+    if year%4==0:
+        print(f"{year} is a leap year")
+    elif year%100==0:
+        print(f"{year} is not a leap year")
+    elif year%400==0:
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+year=int(input("Enter the year:"))
+leapyear(year)
