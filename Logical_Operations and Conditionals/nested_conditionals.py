048277847af330d3a21b2d74e4fe39ffbc07560c
Lenin TK <lenintk1315@gmail.com>
2026-08-05 15:08:50 +0530

    Add nested_conditionals.py

---
+++ Logical_Operations and Conditionals/nested_conditionals.py
@@ -0,0 +1,51 @@
+def nested(side1,side2,side3):
+    def traingle():
+        return side1==side2==side3
+    def isosceles():
+        return side1==side2 or side2==side3 or side1==side3
+    def scalene():
+        return side1!=side2!=side3
+    if traingle():
+        print("The triangle is an equilateral triangle")
+    elif isosceles():
+        print("The triangle is an isosceles triangle")
+    elif scalene():
+        print("The triangle is a scalene triangle")
+    else:
+        print("The triangle is not valid")
+def main():
+    side1=int(input("Enter the first side:"))
+    side2=int(input("Enter the second side:"))
+    side3=int(input("Enter the third side:"))
+    nested(side1,side2,side3)
+main()
+
+
+
+
+
+
+
+
+
+
+# /////////////////////////////////////
+# def triangle(side1,side2,side3):
+#     return side1==side2==side3
+# def isosceles(side1,side2,side3):   
+#     return side1==side2 or side2==side3 or side1==side3
+# def scalene(side1,side2,side3):
+#     return side1!=side2!=side3
+# def main():
+#     side1=int(input("Enter the first side:"))
+#     side2=int(input("Enter the second side:"))
+#     side3=int(input("Enter the third side:"))
+#     if triangle(side1,side2,side3):
+#         print("The triangle is an equilateral triangle")
+#     elif isosceles(side1,side2,side3):
+#         print("The triangle is an isosceles triangle")
+#     elif scalene(side1,side2,side3):
+#         print("The triangle is a scalene triangle")
+#     else:
+#         print("The triangle is not valid")
+# main()
