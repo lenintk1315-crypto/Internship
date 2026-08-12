3c9e7eff31cc8e172d635fc5df02e0672d162ea5
Lenin TK <lenintk1315@gmail.com>
2026-08-12 14:53:17 +0530

    dynamic_collection_management

---
+++ list manipulation/1dynamic_collection_management.py
@@ -0,0 +1,6 @@
+def dynamic_collection_management():
+    list_fruit=["apple","banana","cherry","date","elderberry"]
+    list_fruit.append("strawberry")
+    list_fruit.pop(1)
+    print(list_fruit)
+dynamic_collection_management()
